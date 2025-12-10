f559000cc4e9a2f3a6de0924048cccb287146ba5
Phongdnh-uit <dangnguyenhuyphong@gmail.com>
2025-12-10 12:02:51 +0700

    🐛 fix(server): fix ci test

--- src/test/java/uit/se100/Se100ApplicationTests.java
+++ src/test/java/uit/se100/Se100ApplicationTests.java
@@ -3,11 +3,21 @@ package uit.se100;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(
+    properties = {
+      "spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE",
+      "spring.datasource.driverClassName=org.h2.Driver",
+      "spring.datasource.username=sa",
+      "spring.datasource.password=",
+      "spring.jpa.hibernate.ddl-auto=create-drop",
+      "spring.jpa.show-sql=true",
+      "spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.H2Dialect",
+      "jwt.secret=xin_chao_ban!_day_la_du_an_uit_land_cho_do_an_1_cua_minh!_xin_cam_on_ban_da_doc_du_an_nay",
+      "jwt.refresh-token.expiration=172800",
+      "jwt.access-token.expiration=18000"
+    })
 class Se100ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+  @Test
+  void contextLoads() {}
 }
