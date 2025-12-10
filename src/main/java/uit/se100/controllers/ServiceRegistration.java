e51a9639a9428cc0f26936a6d1901f5e3bed207b
Phongdnh-uit <dangnguyenhuyphong@gmail.com>
2025-12-10 11:25:36 +0700

    🚧 build(server): aircraft

--- src/main/java/uit/se100/controllers/ServiceRegistration.java
+++ src/main/java/uit/se100/controllers/ServiceRegistration.java
@@ -4,11 +4,17 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import uit.se100.dtos.aircraft.AircraftRequest;
+import uit.se100.dtos.aircraft.AircraftResponse;
 import uit.se100.dtos.user.UserRequest;
 import uit.se100.dtos.user.UserResponse;
+import uit.se100.entities.aircraft.Aircraft;
 import uit.se100.entities.authentication.User;
+import uit.se100.hooks.aircraft.AircraftHook;
 import uit.se100.hooks.user.UserHook;
+import uit.se100.mappers.aircraft.AircraftMapper;
 import uit.se100.mappers.user.UserMapper;
+import uit.se100.repositories.aircraft.AircraftRepository;
 import uit.se100.repositories.authentication.UserRepository;
 import uit.se100.services.CrudService;
 import uit.se100.services.GenericService;
@@ -24,5 +30,13 @@ public class ServiceRegistration {
         context.getBean(UserRepository.class),
         context.getBean(UserMapper.class),
         context.getBean(UserHook.class));
+  }
+
+  @Bean
+  CrudService<Aircraft, Long, AircraftRequest, AircraftResponse> aircraftService() {
+    return new GenericService<Aircraft, Long, AircraftRequest, AircraftResponse>(
+        context.getBean(AircraftRepository.class),
+        context.getBean(AircraftMapper.class),
+        context.getBean(AircraftHook.class));
   }
 }
