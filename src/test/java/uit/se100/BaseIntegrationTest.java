c1edfb7a74ddd549997bbafaf9b6d4cdb7f446d8
Phongdnh-uit <dangnguyenhuyphong@gmail.com>
2026-01-12 12:31:51 +0700

    🐛 fix(server): ci & centralize test config

--- src/test/java/uit/se100/BaseIntegrationTest.java
+++ src/test/java/uit/se100/BaseIntegrationTest.java
@@ -1,24 +1,8 @@
 package uit.se100;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(
-    properties = {
-      "spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE",
-      "spring.datasource.driverClassName=org.h2.Driver",
-      "spring.datasource.username=sa",
-      "spring.datasource.password=",
-      "spring.jpa.hibernate.ddl-auto=create-drop",
-      "spring.jpa.show-sql=true",
-      "spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.H2Dialect",
-      "jwt.secret=xin_chao_ban!_day_la_du_an_uit_land_cho_do_an_1_cua_minh!_xin_cam_on_ban_da_doc_du_an_nay",
-      "jwt.refresh-token.expiration=172800",
-      "jwt.access-token.expiration=18000",
-      "spring.mail.host=smtp.gmail.com",
-      "spring.mail.port=587",
-      "spring.mail.username=test@gmail.com",
-      "spring.mail.password=password",
-      "spring.mail.sender=password",
-      "app.client.base-url=http://localhost:3000"
-    })
+@SpringBootTest
+@ActiveProfiles("test")
 public abstract class BaseIntegrationTest {}
